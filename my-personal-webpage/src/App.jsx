aa4a2156611e90195a8cc5e7dc6942ac42862b86
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-06-22 09:12:08 -0600

    removed contact

--- my-personal-webpage/src/App.jsx
+++ my-personal-webpage/src/App.jsx
@@ -3,7 +3,6 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
-import { Contact } from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './App.css';
 
@@ -14,7 +13,6 @@ const App = () => {
       <Banner />
       <Skills />
       <Projects />
-      <Contact />
     </div>
   );
 };
